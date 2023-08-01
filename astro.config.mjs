287b63b5416bacb977632a3aadfd1c4687442f1b
coordova <grcordova@gmail.com>
2023-07-31 20:16:36 -0400

    astro-blog - 14 Build a sitemap / 15 Build an RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'http://localhost:3000/',
   integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp',
-  })]
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), sitemap()]
 });
